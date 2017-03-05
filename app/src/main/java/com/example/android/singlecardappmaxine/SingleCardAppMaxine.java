92e772f7eca8f41b53dab122eee4f3abacb737ff
mesibaja <petra.lehoczky@gmail.com>
2017-03-06 00:11:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
@@ -0,0 +1,13 @@
+package com.example.android.singlecardappmaxine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleCardAppMaxine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_card_app_maxine);
+    }
+}
