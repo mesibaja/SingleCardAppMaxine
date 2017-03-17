9b4314a0eb0a933c3ba2144d310b0f834acae623
mesibaja <petra.lehoczky@gmail.com>
2017-03-17 13:22:26 +0100

    updates

--- app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
+++ app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
@@ -1,13 +1,12 @@
 package com.example.android.singlecardappmaxine;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class SingleCardAppMaxine extends AppCompatActivity {
 
@@ -33,6 +32,21 @@ public class SingleCardAppMaxine extends AppCompatActivity {
         intent.setData(Uri.parse("geo:60.23, -5.19"));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        }
+    }
+
+    public class MyActivity extends Activity {
+        protected void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+
+            setContentView(R.layout.content_layout_id);
+
+            final Button button = (Button) findViewById(R.id.button_id);
+            button.setOnClick(new View.OnClick() {
+                public void onClick(View v) {
+                    // Perform action on click
+                }
+            });
         }
     }
 }
