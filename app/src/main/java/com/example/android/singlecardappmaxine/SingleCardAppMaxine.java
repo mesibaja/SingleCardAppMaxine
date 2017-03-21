dcb7364f2afb91001787c018ce8e5afce7029c83
mesibaja <petra.lehoczky@gmail.com>
2017-03-22 00:21:41 +0100

    updates

--- app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
+++ app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
@@ -1,52 +1,32 @@
 package com.example.android.singlecardappmaxine;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class SingleCardAppMaxine extends AppCompatActivity {
 
+    Intent sendEmailIntent = new Intent(Intent.ACTION_SENDTO,
+            Uri.fromParts("mailto", "maxine@maxinenwaneri.com", null));
+
     @Override
-    public void openWebPage(String url) {
-        Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, www.maxinenwaneri.com);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_card_app_maxine);
     }
 
-    public void dialPhoneNumber(String phoneNumber) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel:" + 2125551212));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+    public void CallNumber(View v) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+4915757717173"));
+        startActivity(dialIntent);
     }
 
-    public void showMap(Uri geoLocation) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("geo:60.23, -5.19"));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+    public void SendEmail(View v) {
     }
 
-    public class MyActivity extends Activity {
-        protected void onCreate(Bundle icicle) {
-            super.onCreate(icicle);
-
-            setContentView(R.layout.content_layout_id);
-
-            final Button button = (Button) findViewById(R.id.button_id);
-            button.setOnClick(new View.OnClick() {
-                public void onClick(View v) {
-                    // Perform action on click
-                }
-            });
+public void GoToWebsite(View v){
+        Intent openWebsite=new Intent(Intent.ACTION_VIEW,Uri.parse("www.maxinenwaneri.com"));
+        startActivity(openWebsite);
         }
-    }
 }
