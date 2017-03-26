ba4b4da89d97507097c9a6f7f71a243071ef0147
mesibaja <petra.lehoczky@gmail.com>
2017-03-26 12:44:23 +0200

    Java Extra text intents updated but give no result

--- app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
+++ app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
@@ -8,9 +8,15 @@ import android.view.View;
 
 public class SingleCardAppMaxine extends AppCompatActivity {
 
+    public void SendEmail(View v) {
     Intent sendEmailIntent = new Intent(Intent.ACTION_SENDTO,
             Uri.fromParts("mailto", "maxine@maxinenwaneri.com", null));
 
+sendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, "Please give me a call back at...");
+    sendEmailIntent.putExtra(Intent.EXTRA_TEXT, "Write your message here.");
+    startActivity(Intent.createChooser(sendEmailIntent, "Choose an email with Gmail etc..."));
+    }
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +26,6 @@ public class SingleCardAppMaxine extends AppCompatActivity {
     public void CallNumber(View v) {
         Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+4915757717173"));
         startActivity(dialIntent);
-    }
-
-    public void SendEmail(View v) {
     }
 
 public void GoToWebsite(View v){
