6d4cfcb40679b858046646eb02e0ca7cf9a5393b
mesibaja <petra.lehoczky@gmail.com>
2017-05-06 01:38:09 +0200

    Buttons, action bar changed

--- app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
+++ app/src/main/java/com/example/android/singlecardappmaxine/SingleCardAppMaxine.java
@@ -2,17 +2,20 @@ package com.example.android.singlecardappmaxine;
 
 import android.content.ContentUris;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 public class SingleCardAppMaxine extends AppCompatActivity {
 
     public void SendEmail(View v) {
         Intent sendEmailIntent = new Intent(Intent.ACTION_SENDTO,
-                Uri.fromParts("mailto", "maxine@maxinenwaneri.com", null));
+                Uri.fromParts("mailto", "support@thefutureisgreater.com", null));
 
         sendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, "Please give me a call back at...");
         sendEmailIntent.putExtra(Intent.EXTRA_TEXT, "Write your message here.");
@@ -26,13 +29,17 @@ public class SingleCardAppMaxine extends AppCompatActivity {
     }
 
     public void OpenCalendar (View v) {
-        Intent openCalenarintent = new Intent(Intent.ACTION_INSERT);
-        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
-        startActivity(openCalenarintent.setData(builder.build()));
+        Intent openCalendar=new Intent(Intent.ACTION_VIEW,Uri.parse("https://maxinenwaneri.satoriapp.com/book"));
+        startActivity(openCalendar);
     }
 
     public void GoToWebsite(View v){
-        Intent openWebsite=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.maxinenwaneri.com"));
+        Intent openWebsite=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.thefutureisgreater.com"));
         startActivity(openWebsite);
+    }
+
+    public void OpenFacebook (View v) {
+        Intent openFacebook=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/groups/TheFutureisGreater/?ref=group_cover"));
+        startActivity(openFacebook);
     }
 }
